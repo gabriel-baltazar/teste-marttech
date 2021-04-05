203c78bd84ea24e96d69fcd52b3fd34d7162be7e
Gabriel Baltazar <gabrielaltazar@hotmail.com>
2021-04-05 12:01:03 -0300

    LocalStorage implementado no carrinho

--- src/Pages/Produtos.js
+++ src/Pages/Produtos.js
@@ -1,31 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Cart from '../components/Cart';
 import Main from '../components/Main';
 import data from '../data';
 
+const cartFromLocalStorage = JSON.parse(localStorage.getItem('cart') || "[]")
+
 const Produtos = () => {
   const { products } = data;
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(cartFromLocalStorage);
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cartItems))
+  }, [cartItems])
+
   const onAdd = (product) => {
+
     const exist = cartItems.find(x => x.id === product.id);
     if (exist) {
       setCartItems(cartItems.map(x => x.id === product.id ? {
         ...exist, qty: exist.qty + 1
       } : x
       ))
+      window.location.reload();
     } else {
       setCartItems([...cartItems, { ...product, qty: 1 }])
+      window.location.reload();
     }
   }
   const onRemove = (product) => {
     const exist = cartItems.find((x) => x.id === product.id);
     if (exist.qty === 1) {
+
       setCartItems(cartItems.filter((x) => x.id !== product.id));
+      window.location.reload();
     } else {
       setCartItems(cartItems.map(x => x.id === product.id ? {
         ...exist, qty: exist.qty - 1
       } : x
       ))
+      window.location.reload();
     }
   }
   return (
--- src/components/Product.js
+++ src/components/Product.js
@@ -4,12 +4,12 @@ const Product = (props) => {
   const { product, onAdd, onRemove } = props;
   return (
     <div className="flex">
-  <div style={{marginBottom:'1rem'}}>
-      <img className="small" src={product.image} alt={product.name}></img>
-      <h3>{product.name}</h3>
-      <p>{product.description}</p>
-      <h3>R$ {product.price}</h3>
-      <span><button onClick={() => onAdd(product)} className="button" >Add</button></span>
+      <div style={{ marginBottom: '1rem' }}>
+        <img className="small" src={product.image} alt={product.name}></img>
+        <h3>{product.name}</h3>
+        <p>{product.description}</p>
+        <h3>R$ {product.price}</h3>
+        <span><button onClick={() => onAdd(product)} className="button" >Add</button></span>
       </div>
     </div>
   )
